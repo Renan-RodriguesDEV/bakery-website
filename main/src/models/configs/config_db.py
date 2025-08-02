9ffd31bfdfbef54f0dab6dca106c93aad505d8ca
Renan-RodriguesDEV <renanrodrigues7110@gmail.com>
2025-08-02 10:08:22 -0300

    fix: remover credenciais de teste do banco de dados do arquivo de configuração

--- main/src/models/configs/config_db.py
+++ main/src/models/configs/config_db.py
@@ -1,9 +1,5 @@
 from streamlit import secrets
 
 configs_db = {
-    "username": secrets["TEST_USER_DB"],
-    "password": secrets["TEST_PASSWORD_DB"],
-    "host": secrets["TEST_HOST_DB"],
-    "database": secrets["TEST_DATABASE_NAME"],
     "connection_url": secrets["DATABASE_URL"],
 }
