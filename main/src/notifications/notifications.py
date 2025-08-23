fa59d80065a966b7c14bb699a2a8f8179e3e29cc
Renan RodriguesDev <renanrodrigues7110@gmail.com>
2025-08-23 01:22:53 -0300

    feat: adicionar carregamento de variáveis de ambiente com dotenv na classe Notifications

--- main/src/notifications/notifications.py
+++ main/src/notifications/notifications.py
@@ -1,4 +1,9 @@
+import os
+
 import pika
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Notifications:
@@ -8,9 +13,14 @@ class Notifications:
         self.username = username
         self.password = password
         self.credentials = pika.PlainCredentials(self.username, self.password)
-        self.connection_params = pika.ConnectionParameters(
-            self.host, self.port, credentials=self.credentials
+        self.connection_params = (
+            pika.ConnectionParameters(
+                self.host, self.port, credentials=self.credentials
+            )
+            if not os.getenv("RABBITMQ_URL")
+            else pika.URLParameters(os.getenv("RABBITMQ_URL"))
         )
+        print(self.connection_params)
         self.channel = self.__create_channel()
 
     def __create_channel(self):
